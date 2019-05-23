95bb8aac946f2b9df7ac1236c996a58ef4a2b12c
kungurtsev <kungurtsev@skbkontur.ru>
2019-05-23 18:29:24 +0500

    init

---
+++ Vostok.Metrics.Aggregations/OneMetricAggregator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Vostok.Metrics.Models;
+
+namespace Vostok.Metrics.Aggregations
+{
+    internal class OneMetricAggregator
+    {
+        public void AddEvent(MetricEvent metric)
+        {
+            
+        }
+
+        public IEnumerable<MetricEvent> GetAggregatedMetrics()
+        {
+            yield break;
+        }
+    }
+}
