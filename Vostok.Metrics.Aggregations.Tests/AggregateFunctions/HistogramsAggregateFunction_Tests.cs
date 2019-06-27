8fc9f88ab0406e279b5aebe6dc08f34765235ba5
kungurtsev <kungurtsev@skbkontur.ru>
2019-06-27 13:33:02 +0500

    dummy histogram

---
+++ Vostok.Metrics.Aggregations.Tests/AggregateFunctions/HistogramsAggregateFunction_Tests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Vostok.Metrics.Aggregations.AggregateFunctions;
+using Vostok.Metrics.Models;
+
+namespace Vostok.Metrics.Aggregations.Tests.AggregateFunctions
+{
+    [TestFixture]
+    internal class HistogramsAggregateFunction_Tests
+    {
+        [Test]
+        public void Aggregate_should_summarize_count_in_buckets()
+        {
+            var function = new HistogramsAggregateFunction();
+
+            var timestamp = DateTimeOffset.Now;
+
+            var aggregationParameters = new Dictionary<string, string>
+            {
+                {"a", "aa"},
+                {"b", "bb"}
+            };
+
+            var tags = MetricTags.Empty
+                .Append("team", "vostok")
+                .Append("project", "metrics-aggregators")
+                .Append(WellKnownTagKeys.Name, "magic");
+
+            for (var i = 0; i < 10; i++)
+                function.AddEvent(new MetricEvent(i, tags, timestamp, "unicorns", WellKnownAggregationTypes.Counter, aggregationParameters));
+
+            //TODO
+            //function.Aggregate(timestamp + 1.Minutes())
+            //    .Should()
+            //    .BeEquivalentTo(
+            //        new MetricEvent(103, tags, timestamp + 1.Minutes(), "unicorns", null, null));
+        }
+    }
+}
---
+++ Vostok.Metrics.Aggregations/AggregateFunctions/HistogramsAggregateFunction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+using Vostok.Metrics.Models;
+
+namespace Vostok.Metrics.Aggregations.AggregateFunctions
+{
+    [PublicAPI]
+    public class HistogramsAggregateFunction : IAggregateFunction
+    {
+        private MetricEvent lastEvent;
+
+        public void AddEvent(MetricEvent @event)
+        {
+            lastEvent = @event;
+            // TODO
+        }
+
+        public IEnumerable<MetricEvent> Aggregate(DateTimeOffset timestamp)
+        {
+            if (lastEvent == null)
+                return Enumerable.Empty<MetricEvent>();
+
+            // TODO
+            return Enumerable.Empty<MetricEvent>();
+        }
+    }
+}
