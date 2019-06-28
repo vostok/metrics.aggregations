a2c0e2929e833b120a75ede869edfcd8264638e9
kungurtsev <kungurtsev@skbkontur.ru>
2019-06-28 12:59:49 +0500

    implement HistogramsAggregateFunction

--- Vostok.Metrics.Aggregations/AggregateFunctions/HistogramsAggregateFunction.cs
+++ Vostok.Metrics.Aggregations/AggregateFunctions/HistogramsAggregateFunction.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Annotations;
 using Vostok.Metrics.Models;
+using Vostok.Metrics.Primitives.Timer;
 
 namespace Vostok.Metrics.Aggregations.AggregateFunctions
 {
@@ -10,20 +11,77 @@ namespace Vostok.Metrics.Aggregations.AggregateFunctions
     public class HistogramsAggregateFunction : IAggregateFunction
     {
         private MetricEvent lastEvent;
+        private Dictionary<HistogramBucket, double> buckets = new Dictionary<HistogramBucket, double>();
 
         public void AddEvent(MetricEvent @event)
         {
             lastEvent = @event;
-            // TODO
+
+            var bucket = @event.AggregationParameters.GetHistogramBucket();
+            if (bucket == null)
+                return;
+
+            if (!buckets.ContainsKey(bucket.Value))
+                buckets[bucket.Value] = 0;
+            buckets[bucket.Value] += @event.Value;
         }
 
         public IEnumerable<MetricEvent> Aggregate(DateTimeOffset timestamp)
         {
-            if (lastEvent == null)
+            if (lastEvent == null || !buckets.Any())
                 return Enumerable.Empty<MetricEvent>();
 
-            // TODO
-            return Enumerable.Empty<MetricEvent>();
+            var result = new List<MetricEvent>();
+            var tags = lastEvent.Tags;
+
+            var quantiles = lastEvent.AggregationParameters.GetQuantiles() ?? Quantiles.DefaultQuantiles;
+            var sortedBuckets = buckets.OrderBy(b => b.Key.LowerBound).ToList();
+
+            var quantileTags = Quantiles.QuantileTags(quantiles, tags);
+            for (var i = 0; i < quantiles.Length; i++)
+            {
+                var value = GetQuantile(sortedBuckets, quantiles[i]);
+                if (value != null)
+                {
+                    result.Add(new MetricEvent(value.Value, quantileTags[i], timestamp, lastEvent.Unit, null, null));
+                }
+            }
+
+            var totalCount = sortedBuckets.Sum(x => x.Value);
+            var countTags = tags.Append(WellKnownTagKeys.Aggregate, WellKnownTagValues.AggregateCount);
+            result.Add(new MetricEvent(totalCount, countTags, timestamp, null, null, null));
+
+            return result;
+        }
+
+        public static double? GetQuantile(List<KeyValuePair<HistogramBucket, double>> sortedBuckets, double quantile)
+        {
+            var totalCount = sortedBuckets.Sum(x => x.Value);
+            var skip = quantile * totalCount;
+
+            var i = 0;
+            while (i < sortedBuckets.Count && sortedBuckets[i].Value < skip)
+            {
+                skip -= sortedBuckets[i].Value;
+                i++;
+            }
+
+            if (i == sortedBuckets.Count)
+                return null;
+
+            var bucket = sortedBuckets[i].Key;
+            var value = sortedBuckets[i].Value;
+
+            if (double.IsPositiveInfinity(bucket.UpperBound))
+                return null;
+
+            if (double.IsNegativeInfinity(bucket.LowerBound))
+                return bucket.UpperBound;
+
+            var length = bucket.UpperBound - bucket.LowerBound;
+            var result = bucket.LowerBound + skip / value * length;
+
+            return result;
         }
     }
 }
